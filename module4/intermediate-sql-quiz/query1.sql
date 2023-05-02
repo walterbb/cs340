ef2428ad9e8318aefd068939e8a8927dafe611bd
walterbb <44916811+walterbb@users.noreply.github.com>
2023-05-01 21:04:46 -0700

    start intermediate quiz

---
+++ module4/intermediate-sql-quiz/query1.sql
@@ -0,0 +1,24 @@
+-- Write the query to create the 4 tables below.
+
+-- Create client table
+CREATE TABLE client (
+    id int(11) NOT NULL AUTO_INCREMENT,
+    first_name varchar(255) NOT NULL,
+    last_name varchar(255) NOT NULL,
+    email varchar(255) NOT NULL,
+    CONSTRAINT full_name UNIQUE (first_name, last_name),
+    PRIMARY KEY (id)
+);
+
+-- Leave the queries below untouched. These are to test your submission correctly.
+-- Test that the tables were created
+DESCRIBE client;
+DESCRIBE employee;
+DESCRIBE project;
+DESCRIBE works_on;
+
+-- Test that the correct foreign keys were created 
+SELECT TABLE_NAME,COLUMN_NAME,CONSTRAINT_NAME,REFERENCED_TABLE_NAME,REFERENCED_COLUMN_NAME 
+FROM INFORMATION_SCHEMA.KEY_COLUMN_USAGE 
+WHERE REFERENCED_TABLE_SCHEMA = 'grade';
+
---
+++ module4/intermediate-sql-quiz/query2.sql
@@ -0,0 +1,8 @@
+-- write your queries to insert data here
+
+
+-- Leave the queries below untouched. These are to test your submission correctly.
+select * from project;
+select * from client;
+select * from employee;
+select * from works_on;
