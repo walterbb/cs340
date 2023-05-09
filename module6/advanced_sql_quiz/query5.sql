bb0f9d0e57b8fd1ef1339de827d0cc627ee6010c
walterbb <44916811+walterbb@users.noreply.github.com>
2023-05-08 21:09:31 -0700

    q5

--- module6/advanced_sql_quiz/query5.sql
+++ module6/advanced_sql_quiz/query5.sql
@@ -4,4 +4,13 @@
 
 
 -- Put your query for q5 here.
-
+SELECT rc.title as film_title 
+FROM (SELECT film.film_id, film.title FROM film JOIN film_actor ON film.film_id = film_actor.film_id
+    JOIN actor ON film_actor.actor_id = actor.actor_id
+    WHERE actor.actor_id = 80) as rc
+JOIN
+(SELECT film.film_id, film.title FROM film JOIN film_actor ON film.film_id = film_actor.film_id
+    JOIN actor ON film_actor.actor_id = actor.actor_id
+    WHERE actor.actor_id = 168) AS ww
+WHERE rc.film_id = ww.film_id
+ORDER BY film_title ASC;
