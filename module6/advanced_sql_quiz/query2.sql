14d60bc7619830fc45fff0484a43837319283abe
walterbb <44916811+walterbb@users.noreply.github.com>
2023-05-08 20:47:10 -0700

    q2 2

--- module6/advanced_sql_quiz/query2.sql
+++ module6/advanced_sql_quiz/query2.sql
@@ -5,11 +5,11 @@
 -- Your query should return every category even if MAE has been in no films in that category
 
 -- Order by the category name in descending order.
-
-SELECT c.category_name, COUNT(fc.film_id) as number_of_films
-FROM category c
-LEFT JOIN film_category fc ON c.category_id = fc.category_id
-LEFT JOIN film_actor fa ON fc.film_id = fa.film_id
-LEFT JOIN actor a ON fa.actor_id = a.actor_id AND a.first_name = 'Mae' AND a.last_name = 'Hoffman'
-GROUP BY c.category_id, c.category_name
-ORDER BY c.category_name DESC;
+SELECT category.name AS category_name, COUNT(actor.actor_id) AS number_of_films
+FROM category
+JOIN film_category ON category.category_id = film_category.category_id
+JOIN film ON film_category.film_id = film.film_id
+JOIN film_actor ON film.film_id = film_actor.film_id
+JOIN actor ON film_actor.actor_id = actor.actor_id AND actor.first_name = 'MAE' AND actor.last_name = 'HOFFMAN'
+GROUP BY category.name
+ORDER BY category.name DESC;
