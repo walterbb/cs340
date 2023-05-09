e37edf7c6d9703b0f856c86bdf271af45acd08c4
walterbb <44916811+walterbb@users.noreply.github.com>
2023-05-08 21:03:14 -0700

    q4

--- module6/advanced_sql_quiz/query4.sql
+++ module6/advanced_sql_quiz/query4.sql
@@ -2,4 +2,14 @@
 -- Order by the actor_id in ascending order.
 
 -- Put your query for q4 here
-
+SELECT actor.first_name, actor.last_name FROM actor
+JOIN film_actor ON actor.actor_id = film_actor.actor_id
+AND actor.actor_id NOT IN
+(SELECT actor.actor_id FROM actor
+JOIN film_actor ON actor.actor_id = film_actor.actor_id
+JOIN film_category ON film_actor.film_id = film_category.film_id
+JOIN category ON film_category.category_id = category.category_id
+JOIN film ON film_actor.film_id = film.film_id
+AND category.category_id = 2)
+GROUP BY actor.actor_id
+ORDER BY actor.actor_id ASC;
