7dd5ca3aec1acdc5eb6a9aadadb16c3b9fe481d0
walterbb <44916811+walterbb@users.noreply.github.com>
2023-05-08 20:59:53 -0700

    q3

--- module6/advanced_sql_quiz/query3.sql
+++ module6/advanced_sql_quiz/query3.sql
@@ -3,5 +3,12 @@
 -- Look at the category table to figure out how to filter data for Animation films.
 -- Order your results by the actor_id in ascending order.
 -- Put query for Q3 here
-
+SELECT actor.first_name, actor.last_name, IFNULL(SUM(film.length), 0) as total_combined_film_length FROM actor
+JOIN film_actor ON actor.actor_id = film_actor.actor_id
+JOIN film_category ON film_actor.film_id = film_category.film_id
+JOIN category ON film_category.category_id = category.category_id
+LEFT JOIN film ON film_actor.film_id = film.film_id
+AND category.category_id = 2
+GROUP BY actor.actor_id
+ORDER BY actor.actor_id ASC;
 
