3d5cd7981d287f53e3ce8ecba6e5ecff45bd1386
walterbb <44916811+walterbb@users.noreply.github.com>
2023-05-08 20:37:25 -0700

    q2

--- module6/advanced_sql_quiz/query2.sql
+++ module6/advanced_sql_quiz/query2.sql
@@ -6,3 +6,9 @@
 
 -- Order by the category name in descending order.
 
+SELECT c.category_name, COUNT(fc.film_id) as number_of_films
+FROM category c
+LEFT JOIN film_category fc ON c.category_id = fc.category_id
+LEFT JOIN film_actor fa ON fc.film_id = fa.film_id
+LEFT JOIN actor a ON fa.actor_id = a.actor_id AND a.first_name = 'Mae' AND a.last_name = 'Hoffman'
+GROUP BY c.category_name;
